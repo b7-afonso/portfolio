f277ceeb6c2fa4d7d3b30de8d8602db2b3833e1a
bafonso <bianca.afonso@wundermanthompson.com>
2024-09-25 14:15:30 +0200

    Navigation layout using Header component

--- biancaafonso/src/Components/header/header.tsx
+++ biancaafonso/src/Components/header/header.tsx
@@ -1,11 +1,30 @@
 function Header () {
   return(
     <div>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/work">Work</a></li>
-      </ul>
+      <div className="card">
+        <div className="row" onclick="handleClick()" role="button" tabindex="0" aria-expanded="false" aria-label="Open Menu">
+          <div className="title">MENU</div>
+          <svg width="32" height="32" viewBox="0 0 100 100" class="icon">
+            <path className="path1">
+              <animate className="animate1" attributeName="d" dur="0.35s" repeatCount="1" fill="freeze" />
+            </path>
+            <path className="path2">
+              <animate className="animate2" attributeName="d" dur="0.35s" repeatCount="1" fill="freeze" />
+            </path>
+            <path className="path3"> 
+              <animate className="animate3" attributeName="d" dur="0.35s" repeatCount="1" fill="freeze" />
+            </path>
+          </svg>
+        </div>
+      </div>
+      <nav className="main-navigation">
+      <div className="logo">Bianca Afonso</div>
+        <ul>
+          <li className="menu-item"><a href="/">Home</a></li>
+          <li className="menu-item"><a href="/about">About</a></li>
+          <li className="menu-item"><a href="/work">Work</a></li>
+        </ul>
+      </nav>
     </div>
   )
 }
--- biancaafonso/src/Pages/home.tsx
+++ biancaafonso/src/Pages/home.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import Header from "../Components/header/header";
+import '../App.css';
 
 function Home() {
     return (
         <div>
-            <h1>Hi! I’m Bianca!</h1>
-            <h2>I turn complex designs into seamless user experiences with creativity and clean code.</h2>
+            <h1 className="hero-heading">Hi! I’m Bianca!</h1>
+            <h2 className="hero-italic">I turn complex designs into seamless user experiences with creativity and clean code.</h2>
         </div>
     )
 }
